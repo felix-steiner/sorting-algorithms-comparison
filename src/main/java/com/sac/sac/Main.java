3dbfe845b3f8deaacfb9923800d49ecec920a3be
Felix Steiner <felix.steiner7@gmail.com>
2021-04-02 12:54:04 +0200

    Implemented the logic of executing and measuring the found algorithms

--- src/main/java/com/sac/sac/Main.java
+++ src/main/java/com/sac/sac/Main.java
@@ -1,5 +1,6 @@
 package com.sac.sac;
 
+import com.google.common.base.Stopwatch;
 import com.sac.sac.algorithms.SortingAlgorithm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,13 +12,23 @@ public class Main {
 
     public static void main(String[] args) {
         logger.info("### Started Sorting Algorithms Comparison application ###");
-        final ReflectionHandler reflectionHandler = new ReflectionHandler();
 
+        int min = -10000;
+        int max = 10000;
+        int size = 10000;
+        logger.info("Generating random numbers for testing the algorithms: min. {}, max. {}, size {}", min, max, size);
+        NumberGenerator numberGenerator = new NumberGenerator();
+        int[] numbers = numberGenerator.generateNumbers(min, max, size);
+
+        final ReflectionHandler reflectionHandler = new ReflectionHandler();
         Set<Class<? extends SortingAlgorithm>> classes = reflectionHandler.loadClasses("com.sac.sac.algorithms");
 
+        Stopwatch stopwatch = Stopwatch.createUnstarted();
         for (SortingAlgorithm sa : reflectionHandler.constructClasses(classes)) {
-            // TODO: measure time
-            sa.sort(null);
+            stopwatch.start();
+            sa.sort(numbers);
+            stopwatch.stop();
+            logger.info("{} sorted the collection in {} ns", sa.getClass().getName(), stopwatch.elapsed().getNano());
         }
     }
 }
